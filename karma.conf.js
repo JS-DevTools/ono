991140e2f68cda60f737ff300237a484884c3cb0
James Messinger <james.messinger@gmail.com>
2017-06-01 08:20:09 -0500

    removed Phantom JS

--- karma.conf.js
+++ karma.conf.js
@@ -80,13 +80,13 @@ function configureLocalBrowsers (config) {
   var isLinux = !isMac && !isWindows;
 
   if (isMac) {
-    config.browsers = ['PhantomJS', 'Firefox', 'Chrome', 'Safari'];
+    config.browsers = ['Firefox', 'Chrome', 'Safari'];
   }
   else if (isLinux) {
-    config.browsers = ['PhantomJS', 'Firefox'];
+    config.browsers = ['Firefox'];
   }
   else if (isWindows) {
-    config.browsers = ['PhantomJS', 'Firefox', 'Chrome', 'IE9', 'IE10', 'IE'];
+    config.browsers = ['Firefox', 'Chrome', 'IE9', 'IE10', 'IE'];
 
     // NOTE: IE 6, 7, 8 are not supported by Chai
     config.customLaunchers = {
