6bfbd8099fe6d043e00f3f8739bac5e8f7f04834
James Messinger <james.messinger@gmail.com>
2017-11-10 09:03:44 -0800

    changed the author URL

--- dist/ono.js
+++ dist/ono.js
@@ -1,9 +1,9 @@
 /*!
  * Ono v4.0.2 (July 9th 2017)
- * 
+ *
  * https://github.com/bigstickcarpet/ono
- * 
- * @author  James Messinger (http://jamesmessinger.com)
+ *
+ * @author  James Messinger (http://bigstickcarpet.com)
  * @license MIT
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ono = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -328,7 +328,7 @@ function format(fmt) {
           break;
       }
       if(!escaped) {
-        return arg; 
+        return arg;
       }
       args.unshift(arg);
       return match;
