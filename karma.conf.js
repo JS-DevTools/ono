ae23d9ea859463b3ff52d212bf06b9ab7d8dc5e3
James Messinger <james.messinger@gmail.com>
2018-12-06 08:05:34 -0600

    Experimenting with Windows testing in CI

--- karma.conf.js
+++ karma.conf.js
@@ -113,11 +113,6 @@ function configureSauceLabs (config) {
   };
 
   config.customLaunchers = {
-    SauceLabs_Safari_Latest: {
-      base: 'SauceLabs',
-      platform: 'macOS 10.12',
-      browserName: 'safari'
-    },
     SauceLabs_IE_11: {
       base: 'SauceLabs',
       platform: 'Windows 7',
@@ -127,16 +122,6 @@ function configureSauceLabs (config) {
       base: 'SauceLabs',
       platform: 'Windows 10',
       browserName: 'microsoftedge'
-    },
-    SauceLabs_Chrome_Latest: {
-      base: 'SauceLabs',
-      platform: 'Windows 10',
-      browserName: 'chrome'
-    },
-    SauceLabs_Firefox_Latest: {
-      base: 'SauceLabs',
-      platform: 'Windows 10',
-      browserName: 'firefox'
     },
   };
 
