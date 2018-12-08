2286cb7aa119580c378c6634e3090520c70c47ff
James Messinger <james.messinger@gmail.com>
2018-12-08 08:58:33 -0600

    Cleaned-up the CI scripts for cross-platform and cross-browser testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,69 +1,53 @@
 // Karma config
 // https://karma-runner.github.io/0.12/config/configuration-file.html
 
-'use strict';
+"use strict";
 
 module.exports = function (karma) {
-  var config = {
-    frameworks: ['mocha', 'chai', 'host-environment'],
-    reporters: ['verbose'],
+  let config = {
+    frameworks: ["mocha", "chai", "host-environment"],
+    reporters: ["verbose"],
 
     files: [
       // Ono
-      'dist/ono.min.js',
-      { pattern: 'dist/*.map', included: false, served: true },
+      "dist/ono.min.js",
+      { pattern: "dist/*.map", included: false, served: true },
 
       // Test Fixtures
-      'test/fixtures/*.js',
+      "test/fixtures/*.js",
 
       // Test Specs
-      'test/specs/*.spec.js'
+      "test/specs/*.spec.js"
     ],
   };
 
-  exitIfDisabled();
   configureCodeCoverage(config);
-  configureLocalBrowsers(config);
-  configureSauceLabs(config);
+  configureBrowsers(config);
 
-  console.log('Karma Config:\n', JSON.stringify(config, null, 2));
+  console.log("Karma Config:\n", JSON.stringify(config, null, 2));
   karma.set(config);
 };
-
-/**
- * If this is a CI job, and Karma is not enabled, then exit.
- * (useful for CI jobs that are only testing Node.js, not web browsers)
- */
-function exitIfDisabled () {
-  var CI = process.env.CI === 'true';
-  var KARMA = process.env.KARMA === 'true';
-
-  if (CI && !KARMA) {
-    console.warn('Karma is not enabled');
-    process.exit();
-  }
-}
 
 /**
  * Configures the code-coverage reporter
  */
 function configureCodeCoverage (config) {
-  if (process.argv.indexOf('--coverage') === -1) {
-    console.warn('Code-coverage is not enabled');
+  if (process.argv.indexOf("--coverage") === -1) {
+    console.warn("Code-coverage is not enabled");
     return;
   }
 
-  config.reporters.push('coverage');
+  config.reporters.push("coverage");
   config.coverageReporter = {
     reporters: [
-      { type: 'text-summary' },
-      { type: 'lcov' }
+      { type: "text-summary" },
+      { type: "lcov" }
     ]
   };
 
   config.files = config.files.map(function (file) {
-    if (typeof file === 'string') {
-      file = file.replace(/^dist\/(.*?)(\.min)?\.js$/, 'dist/$1.coverage.js');
+    if (typeof file === "string") {
+      file = file.replace(/^dist\/(.*?)(\.min)?\.js$/, "dist/$1.coverage.js");
     }
     return file;
   });
@@ -72,65 +56,74 @@ function configureCodeCoverage (config) {
 /**
  * Configures the browsers for the current platform
  */
-function configureLocalBrowsers (config) {
-  var isMac = /^darwin/.test(process.platform);
-  var isWindows = /^win/.test(process.platform);
-  var isLinux = !isMac && !isWindows;
+function configureBrowsers (config) {
+  let isMac = /^darwin/.test(process.platform);
+  let isWindows = /^win/.test(process.platform);
+  let isLinux = !isMac && !isWindows;
+  let isCI = process.env.CI === "true";
 
-  if (isMac) {
-    config.browsers = ['FirefoxHeadless', 'ChromeHeadless', 'Safari'];
+  if (isCI) {
+    if (isMac) {
+      config.browsers = ["FirefoxHeadless", "ChromeHeadless", "Safari"];
+    }
+    else if (isLinux) {
+      config.browsers = ["FirefoxHeadless", "ChromeHeadless"];
+    }
+    else if (isWindows) {
+      // IE and Edge aren't available in CI, so use SauceLabs
+      configureSauceLabs(config);
+    }
+  }
+  else if (isMac) {
+    config.browsers = ["Firefox", "Chrome", "Safari"];
   }
   else if (isLinux) {
-    config.browsers = ['FirefoxHeadless', 'ChromeHeadless'];
+    config.browsers = ["Firefox", "Chrome"];
   }
   else if (isWindows) {
-    config.browsers = ['FirefoxHeadless', 'ChromeHeadless', 'IE', 'Edge'];
+    config.browsers = ["Firefox", "Chrome", "IE", "Edge"];
   }
 }
 
 /**
- * Configures Sauce Labs emulated browsers/devices.
+ * Configures Sauce Labs to test on IE and Edge.
  * https://github.com/karma-runner/karma-sauce-launcher
  */
 function configureSauceLabs (config) {
-  var SAUCE = process.env.SAUCE === 'true';
-  var username = process.env.SAUCE_USERNAME;
-  var accessKey = process.env.SAUCE_ACCESS_KEY;
+  let username = process.env.SAUCE_USERNAME;
+  let accessKey = process.env.SAUCE_ACCESS_KEY;
 
-  if (!SAUCE || !username || !accessKey) {
-    console.warn('SauceLabs is not enabled');
-    return;
+  if (!username || !accessKey) {
+    throw new Error(`SAUCE_USERNAME and/or SAUCE_ACCESS_KEY is not set`);
   }
 
-  var project = require('./package.json');
-  var testName = project.name + ' v' + project.version;
-  var build = testName + ' Build #' + process.env.TRAVIS_JOB_NUMBER + ' @ ' + new Date();
+  let project = require("./package.json");
 
   config.sauceLabs = {
-    build: build,
+    build: `${project.name} v${project.version} Build #${jobNumber}`,
     testName: testName,
     tags: [project.name],
   };
 
   config.customLaunchers = {
-    SauceLabs_IE_11: {
-      base: 'SauceLabs',
-      platform: 'Windows 7',
-      browserName: 'internet explorer'
+    IE_11: {
+      base: "SauceLabs",
+      platform: "Windows 7",
+      browserName: "internet explorer"
     },
-    SauceLabs_IE_Edge: {
-      base: 'SauceLabs',
-      platform: 'Windows 10',
-      browserName: 'microsoftedge'
+    Edge: {
+      base: "SauceLabs",
+      platform: "Windows 10",
+      browserName: "microsoftedge"
     },
   };
 
-  config.reporters.push('saucelabs');
+  config.reporters.push("saucelabs");
   config.browsers = Object.keys(config.customLaunchers);
   // config.concurrency = 1;
   config.captureTimeout = 60000;
   config.browserDisconnectTolerance = 5,
   config.browserDisconnectTimeout = 60000;
   config.browserNoActivityTimeout = 60000;
-  // config.logLevel = 'debug';
+  // config.logLevel = "debug";
 }
