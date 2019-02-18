b7f678f2cd9235fff6acbfee23abacd572f13903
James Messinger <james.messinger@gmail.com>
2019-02-18 07:12:28 -0600

    Separated the serialization logic into its own file

--- src/extend-error.ts
+++ src/extend-error.ts
@@ -1,8 +1,6 @@
+import { errorInspect, errorToJSON, getDeepKeys } from "./serialization";
 import { hasLazyStack, joinStacks, lazyJoinStacks } from "./stack";
 import { ErrorPOJO, OnoError } from "./types";
-
-const nonJsonTypes = ["function", "symbol", "undefined"];
-const objectPrototype = Object.getPrototypeOf({});
 
 /**
  * Extends the new error with the properties of the original error and the `props` object.
@@ -22,7 +20,7 @@ export function extendError(newError: OnoError, originalError?: ErrorPOJO, props
   newError.toJSON = errorToJSON;
 
   // Add an inspect() method, for compatibility with Node.js' `util.inspect()` method
-  newError.inspect = errorToString;
+  newError.inspect = errorInspect;
 
   // Copy custom properties, possibly including custom `toJSON()` and/or `inspect()` methods
   extend(newError, props, true);
@@ -64,56 +62,4 @@ function extend(newError: OnoError, props?: object, overwrite = false) {
       }
     }
   }
-}
-
-/**
- * Custom JSON serializer for Error objects.
- * Returns all built-in error properties, as well as extended properties.
- */
-function errorToJSON(this: OnoError): ErrorPOJO {
-  let json: ErrorPOJO = {};
-
-  for (let key of getDeepKeys(this)) {
-    // @ts-ignore - https://github.com/Microsoft/TypeScript/issues/1863
-    let value = this[key];
-    let type = typeof value;
-
-    if (!nonJsonTypes.includes(type)) {
-      // @ts-ignore - https://github.com/Microsoft/TypeScript/issues/1863
-      json[key] = value;
-    }
-  }
-
-  return json;
-}
-
-/**
- * Serializes Error objects as human-readable JSON strings for debugging/logging purposes.
- */
-function errorToString(this: OnoError) {
-  return JSON.stringify(this, undefined, 2).replace(/\\n/g, "\n");
-}
-
-/**
- * Returns own, inherited, enumerable, non-enumerable, string, and symbol keys of `obj`.
- * Does NOT return members of the base Object prototype or members that could alter the prototype
- * chain, such as "prototype", "__proto__", "constructor", etc.
- */
-function getDeepKeys(obj: object): Set<string | symbol> {
-  let keys: Array<string | symbol> = [];
-
-  while (obj && obj !== objectPrototype) {
-    keys = keys.concat(
-      Object.getOwnPropertyNames(obj),
-      Object.getOwnPropertySymbols(obj),
-    );
-    obj = Object.getPrototypeOf(obj) as object;
-  }
-
-  let uniqueKeys = new Set(keys);
-  uniqueKeys.delete("constructor");
-  uniqueKeys.delete("prototype");
-  uniqueKeys.delete("__proto__ ");
-
-  return uniqueKeys;
 }
