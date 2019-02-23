531794e83dd5ef626435017b834cf7cc03336ade
James Messinger <james.messinger@gmail.com>
2019-02-23 10:07:09 -0600

    Fixed a copy-and-paste mistake

--- test/utils/onoes.js
+++ test/utils/onoes.js
@@ -5,10 +5,10 @@ const { ono } = require("../../");
 module.exports = [
   { name: "ono", ono, ErrorType: Error, errorTypeName: "Error" },
   { name: "ono.error", ono: ono.error, ErrorType: Error, errorTypeName: "Error" },
-  { name: "ono.error", ono: ono.eval, ErrorType: EvalError, errorTypeName: "EvalError" },
-  { name: "ono.error", ono: ono.range, ErrorType: RangeError, errorTypeName: "RangeError" },
-  { name: "ono.error", ono: ono.reference, ErrorType: ReferenceError, errorTypeName: "ReferenceError" },
-  { name: "ono.error", ono: ono.syntax, ErrorType: SyntaxError, errorTypeName: "SyntaxError" },
-  { name: "ono.error", ono: ono.type, ErrorType: TypeError, errorTypeName: "TypeError" },
-  { name: "ono.error", ono: ono.uri, ErrorType: URIError, errorTypeName: "URIError" },
+  { name: "ono.eval", ono: ono.eval, ErrorType: EvalError, errorTypeName: "EvalError" },
+  { name: "ono.range", ono: ono.range, ErrorType: RangeError, errorTypeName: "RangeError" },
+  { name: "ono.reference", ono: ono.reference, ErrorType: ReferenceError, errorTypeName: "ReferenceError" },
+  { name: "ono.syntax", ono: ono.syntax, ErrorType: SyntaxError, errorTypeName: "SyntaxError" },
+  { name: "ono.type", ono: ono.type, ErrorType: TypeError, errorTypeName: "TypeError" },
+  { name: "ono.uri", ono: ono.uri, ErrorType: URIError, errorTypeName: "URIError" },
 ];
