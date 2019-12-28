7cc1accb68bcfe5a37c5169f5c51675bda2ff269
James Messinger <james.messinger@gmail.com>
2019-12-28 08:01:37 -0600

    Changed the default `ono()` function to try to match the original error type, rather than simply using the base `Error` class

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,8 @@
-import { formatter } from "./isomorphic.node";
-import { Ono, ono } from "./ono";
-import { toJSON as _toJSON } from "./to-json";
-import { ErrorLike, ErrorPOJO } from "./types";
+import { ono } from "./singleton";
 
-// Create Ono instances for each of the JavaScript error types
-ono.error = new Ono(Error);
-ono.eval = new Ono(EvalError);
-ono.range = new Ono(RangeError);
-ono.reference = new Ono(ReferenceError);
-ono.syntax = new Ono(SyntaxError);
-ono.type = new Ono(TypeError);
-ono.uri = new Ono(URIError);
-
-// Default to Node's `util.format()` functionality, but allow users to substitute their own
-ono.formatter = formatter;
-
-/**
- * Returns an object containing all properties of the given Error object,
- * which can be used with `JSON.stringify()`.
- */
-Ono.toJSON = function toJSON<E extends ErrorLike>(error: E) {
-  return _toJSON.call(error) as ErrorPOJO & E;
-};
-
-// Export type definitions
+export { Ono } from "./constructor";
 export * from "./types";
-// Export the Ono singleton and the Ono constructor as named exports
-export { ono, Ono };
-
+export { ono };
 
 // tslint:disable-next-line: no-default-export
 export default ono;
