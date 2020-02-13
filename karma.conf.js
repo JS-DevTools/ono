cb439344f5ed3c0c40a86fa6a335886d27c2c774
James Messinger <james.messinger@gmail.com>
2020-02-13 06:48:42 -0600

    Switched from Travis CI to GitHub Actions

--- karma.conf.js
+++ karma.conf.js
@@ -3,12 +3,17 @@
 // https://jstools.dev/karma-config/
 
 "use strict";
-const { karmaConfig } = require("karma-config");
+const { karmaConfig } = require("@jsdevtools/karma-config");
+const { host } = require("@jsdevtools/host-environment");
 
 module.exports = karmaConfig({
   sourceDir: "esm",
   fixtures: "test/fixtures/**/*.js",
   browsers: {
-    ie: true,
+    chrome: true,
+    firefox: host.os.linux,
+    safari: host.os.linux,    // SauceLabs
+    edge: host.os.linux,      // SauceLabs
+    ie: host.os.windows,
   },
 });
