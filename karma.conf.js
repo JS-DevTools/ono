c5ba66a763e0b47f1838a00b99e3807bf2d38fcb
James Messinger <james.messinger@gmail.com>
2020-07-08 05:19:19 -0500

    IE needs to run by itself, due to Babel transforms

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,6 @@ module.exports = karmaConfig({
     firefox: host.ci ? host.os.linux : true,
     safari: host.ci ? host.os.linux : host.os.mac,    // SauceLabs in CI
     edge: host.ci ? host.os.linux : host.os.windows,  // SauceLabs in CI
-    ie: host.os.windows,
+    ie: host.ci ? host.os.windows : false,  // IE needs to run by itself, due to Babel transforms
   },
 });
