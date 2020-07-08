3b1bd865f64f2cb96af8e940bb4fb42c46c681c4
James Messinger <james.messinger@gmail.com>
2020-07-08 06:38:58 -0500

    Fixed tests that were failing on Safari

--- test/utils/compare-keys.js
+++ test/utils/compare-keys.js
@@ -16,48 +16,51 @@ function compareKeys (...expected) {
    * @returns {boolean}
    */
   return function (error) {
-    let actual = [];
+    let actual = new Set();
 
     // Make sure all the specified keys exist
     for (let key of expected) {
       if (key in error) {
-        actual.push(key);
+        actual.add(key);
       }
     }
 
     // Make sure the error doesn't have any extra keys
     // eslint-disable-next-line guard-for-in
     for (let key in error) {
-      if (!actual.includes(key)) {
-        actual.push(key);
-      }
+      actual.add(key);
     }
 
     if (host.error.hasColumn && "column" in error) {
+      actual.add("column");
       expected.push("column");
     }
 
     if (host.error.hasLine && "line" in error) {
+      actual.add("line");
       expected.push("line");
     }
 
     if (host.error.hasSourceURL && "sourceURL" in error) {
+      actual.add("sourceURL");
       expected.push("sourceURL");
     }
 
     if (host.error.hasEnumerableDescription && "description" in error) {
+      actual.add("description");
       expected.push("description");
     }
 
     try {
+      actual = [...actual];
       expect(actual).to.have.same.members(expected);
     }
     catch (e) {
       console.error(`
-EXPECTED:
+EXPECTED KEYS:
   ${expected.join("\n  ")}
 
-ACTUAL:
+ACTUAL KEYS:
   ${actual.join("\n  ")}
 `);
       throw e;
