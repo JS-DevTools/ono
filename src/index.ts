c00f5c42090f43764313f7df314aaf4ff1408276
Titus Stone <titus.stone@ibotta.com>
2023-06-16 13:51:32 -0600

    Revert "Verify default exists before extending it"
    This reverts commit 709651a0de373ca2db143b91cb53793d1c2e0e2e.

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ export { ono };
 export default ono;
 
 // CommonJS default export hack
-if (typeof module === "object" && typeof module.exports === "object" && typeof module.exports.default == "object") {
+if (typeof module === "object" && typeof module.exports === "object") {
   module.exports = Object.assign(module.exports.default, module.exports);
 }
